0e10bf90b1fdeefe3c9ef7a37b71a096e345f749
Rafiqul <Rafiqul.qzsw@gmail.com>
2026-09-04 18:21:50 +0100

    upgrade game library with file persistence and editing

--- game_library.py
+++ game_library.py
@@ -1,12 +1,56 @@
-games = {"terraria": 10, "minecraft": 8, "gta": 9}
+games = {}
+import os
 
+FILE = os.path.join(os.path.dirname(__file__), "test.txt")
+
+with open(FILE) as file:
+    for line in file:
+        if line.strip():
+            game, rating = line.strip().split(",")
+            games[game] = int(rating)
+
+def save_games():
+    with open(FILE, "w") as file:
+        for game,rating in games.items():
+            file.write(f"{game},{rating}\n")
+
+def get_rating():
+    while True:
+        try:
+            rating = int(input("Enter your rating!"))
+            if rating in range(1,11):
+                print("game added!")
+                return rating
+            else:
+                print("Rating must be between 1 and 10")
+                continue
+        except ValueError:
+            print("that wont work lad!")
+            continue
+
+def edit_rating():
+    game = input("Enter your game")
+    if game in games:
+        rating = get_rating()
+        games[game] = rating
+        save_games()
+    else:
+        print("You dont have that game!")
+        answer = input("Would you like to add it?")
+        if answer == "no":
+            print("Then no change will be made.")
+        elif answer == "yes":
+            rating = get_rating()
+            games[game] = rating
+            save_games()
 
 def menu():
     print("1.Show all games")
     print("2.Add a game")
     print("3.Find a game")
     print("4.Remove a game")
-    print("5.quit")
+    print("5.edit a rating")
+    print("6.quit")
 
 def show_games():
     for game, rating in games.items():
@@ -18,20 +62,12 @@ def add_game():
 
     if game in games:
         return False
-    elif game not in games:
-        while True:
-            try:
-                rating = int(input("Enter your rating!"))
-                if rating in range(1,11):
-                    print("game added!")
-                    break
-                else:
-                    print("Rating must be between 1 and 10")
-                    continue
-            except ValueError:
-                print("that wont work lad!")
-                continue
+    else:
+        rating = get_rating()
         games[game] = rating
+        save_games()
+
+
         return True
 
 def find_game():
@@ -46,6 +82,9 @@ def delete_game():
     if game in games:
         del games[game]
         print(f"{game} deleted successfully!")
+        save_games()
+
+
     else:
         print("that game isnt in your library")
     
@@ -82,10 +121,7 @@ while True:
         delete_game()
 
     elif number == 5:
+        edit_rating()
+
+    elif number == 6:
         break
-
-
-
-
-
-    
